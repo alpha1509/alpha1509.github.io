f6e6a936f8b7bb307367f635835971926b290862
alpha1509 <panupong.chimmai@kkumail.com>
2020-09-15 23:45:38 +0700

    add problem2 file

---
+++ lab5-2020/lib/problem2.js
@@ -0,0 +1,71 @@
+function isPosInteger (inputNum) {
+    if (!isNaN(inputNum) && !inputNum.includes(".")) {
+        var realNum = parseFloat(inputNum);
+        if(Number.isInteger(realNum) && realNum > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+function isNegInteger (inputNum) {
+    if (!isNaN(inputNum) && !inputNum.includes(".")) {
+        var realNum = parseFloat(inputNum);
+        if(Number.isInteger(realNum) && realNum < 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+function isNotEmptyArray(array) {
+    if (array.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function readInput() {
+    let numArray = [];
+    while (true) {
+        let inputNum = prompt('Enter an integer (a negative integer to quit)');
+        if (isPosInteger(inputNum)) {
+            n = parseInt(inputNum);
+            numArray.push(n);
+        } else if (isNegInteger(inputNum)) {
+            break;
+        }
+    }
+    return numArray;
+}
+
+function calAverageArrayNum(list) {
+    let x = 0;
+    for (i = 0; i < list.length; i++) {
+        x = x + list[i];
+    }
+    return (x / list.length);
+}
+
+function displayStats(list) {
+    if (isNotEmptyArray(list)) {
+        let originalList = list.slice();
+        list.sort(function(a, b){return a - b});
+        let avg = calAverageArrayNum(list);
+        let min = list[0];
+        let max = list[list.length - 1];  
+        return alert("For the list " + originalList + ", the average is " + avg + ", the minimum is " + min + ", the maximum is " + max) //use <toFixed(n)> to fixed n decimal point
+    } else {
+        return alert("For the list that is empty, the average is 0, the minimum is 0, the maximum is 0")
+    }
+}
+
+let list = readInput();
+displayStats(list);
