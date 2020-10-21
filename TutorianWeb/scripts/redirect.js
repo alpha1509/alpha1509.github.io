1b4b463d09f01abc5194942d59dfc3188e9eaaf4
alpha1509 <panupong.chimmai@kkumail.com>
2020-10-21 16:07:02 +0700

    test responsive Tutorian

---
+++ TutorianWeb/scripts/redirect.js
@@ -0,0 +1,10 @@
+
+ document.getElementsByClassName("coursesTab")[0].onclick = function () {
+    location.href = "http://127.0.0.1:5500/courses.html";
+ };
+ document.getElementsByClassName("calendarTab")[0].onclick = function () {
+    location.href = "http://127.0.0.1:5500/calendar.html";
+ };
+ document.getElementsByClassName("statTab")[0].onclick = function () {
+    location.href = "http://127.0.0.1:5500/statistics.html";
+ };
